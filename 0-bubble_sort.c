ff4f683f0c5dc62331820c14ac397a8bdf40f93b
Obony <faithnyabz69@gmail.com>
2023-04-24 13:10:42 -0700

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,45 @@
 #include "sort.h"
-#include <stdbool.h>
 
 /**
- * bubble_sort - sorts array of integers in ascending order
- * @array: given array
- * @size: size of array
+ * swap_values - Swaps two integers in array.
+ * @x: First integer to swap.
+ * @z: Second integer to swap.
+ */
+void swap_values(int *x, int *z)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *z;
+	*z = tmp;
+}
+/**
+ * bubble_sort - Sorts an array containing integers ascending order.
+ * @array: An array of integers to be sorted.
+ * @size: The size of array.
  *
- * Description: this function sorts an array of integers
- * in increasing order using the Bubble sort algorithm
- *
- * Return: void
+ * Description: Prints array after every swap being done.
  */
 void bubble_sort(int *array, size_t size)
 {
-	int tmp;
-	size_t i, j;
-	bool swapped;
+	size_t index, current_size = size;
+	bool is_sorted = false;
 
-	if (!array)
+	if (array == NULL || size < 2)
 		return;
 
-	swapped = 1;
-	for (i = 0; i < (size - 1) && swapped; i++)
+	while (is_sorted == false)
 	{
-		swapped = 0;
-		for (j = 0; j < (size - 1 - i); j++)
+		is_sorted = true;
+		for (index = 0; index < current_size - 1; index++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[index] > array[index + 1])
 			{
-				/* swap the two elements */
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-
-				swapped = 1;
+				swap_values(array + index, array + index + 1);
 				print_array(array, size);
+				is_sorted = false;
 			}
 		}
+		current_size--;
 	}
 }
